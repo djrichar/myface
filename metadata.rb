390d94aa89565f7da6b266cfb5540f3d4d5710bd
Daniel <daniel_richardson@intuit.com>
2013-06-18 12:36:11 -0700

    update

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'daniel.tester@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures danielr928-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
