40ead453fed0d5df44d0fea9ef5870281a12cf68
Daniel <daniel_richardson@intuit.com>
2013-06-18 12:06:33 -0700

    fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'myface'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'danielr'
+maintainer_email 'daniel.tester@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
